5ac4aa86304a48e71f788d5ff97f4d42b102055c
Dmitriy Fomin <79067773397@yandex.ru>
2021-01-18 04:43:27 +0300

    level 18.1

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task18.task1804;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Самые редкие байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
