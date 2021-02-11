ec529e8cce8b536659311ad235cdc325d082f6ca
Dmitriy Fomin <79067773397@yandex.ru>
2021-02-11 07:27:29 +0300

    lesson20.5

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2026/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.task20.task2026;
 Алгоритмы-прямоугольники
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         byte[][] a1 = new byte[][]{
@@ -26,6 +29,37 @@ public class Solution {
     }
 
     public static int getRectangleCount(byte[][] a) {
-        return 0;
+
+        int rectangleCount=0;
+        int length = a.length;
+        List<String> list = new ArrayList<>();
+        String cell;
+        boolean isRectangle = false;
+        for (int i = 0; i < length; i++) {
+
+            for (int j = 0; j < length; j++) {
+               cell=i+" "+j;
+                if (a[i][j] == 1 && (!list.contains(i+" "+j))) {
+                    isRectangle = true;
+                    list.add(cell);
+                    for (int k = i + 1; k < length; k++) {
+                        if (a[k][j] == 1) list.add(k+" "+j);
+                        else k = length;
+                    }
+                } else {
+                    if (isRectangle){
+                        isRectangle=false;
+                        rectangleCount++;
+                    }
+                }
+            }
+            if(isRectangle){
+                isRectangle=false;
+                rectangleCount++;
+            }
+        }
+
+        return rectangleCount;
     }
+
 }
