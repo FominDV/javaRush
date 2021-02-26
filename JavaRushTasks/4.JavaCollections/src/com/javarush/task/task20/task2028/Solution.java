03209650ce821c732232cb1600b08c428a0939e1
Dmitriy Fomin <79067773397@yandex.ru>
2021-02-26 07:25:00 +0300

    collection1.8

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task20.task2028;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.sql.*;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        List<String> list = new CustomTree();
+    }
+}
--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -4,12 +4,14 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 /* 
 Читаем конфиги
 */
 
 public class Solution {
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
         Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
