e2a263a8d29cd4ba7eaa232488ec23d94510bc68
Dmitriy Fomin <79067773397@yandex.ru>
2021-04-22 20:31:13 +0300

    collection2.9

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task32.task3212;
+
+import com.javarush.task.task32.task3212.contex.InitialContext;
+import com.javarush.task.task32.task3212.service.Service;
+
+public class ServiceLocator {
+    private static Cache cache;
+    private static InitialContext initialContext;
+
+    static {
+        cache = new Cache();
+        initialContext = new InitialContext();
+    }
+
+    /**
+     * First, check for a service object in the cache
+     * If a service object is not in the cache, perform a lookup using
+     * the JNDI initial context and get the service object. Add it to
+     * the cache for future use.
+     *
+     * @param jndiName The name of the service object in the context
+     * @return Object mapped to the name in context
+     */
+    public static Service getService(String jndiName) {
+        Service service = cache.getService(jndiName);
+        if (service != null) {
+            return service;
+        }
+        service = (Service) initialContext.lookup(jndiName);
+        cache.addService(service);
+        return service;
+    }
+}
