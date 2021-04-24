67ec7677ee03ff9006d32ed3bcfc08cfe4cb0d67
Dmitriy Fomin <79067773397@yandex.ru>
2021-04-24 21:41:19 +0300

    collection 2.11

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -5,6 +5,14 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
         
     }
