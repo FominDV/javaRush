7920f6833b8dd274848e12740c63dacba56594e3
Dmitriy Fomin <79067773397@yandex.ru>
2021-07-01 17:29:46 +0300

    game3

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,7 +1,51 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Space {
+
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private List<Bomb> bombs = new ArrayList<>();
+    private List<Ufo> ufos = new ArrayList<>();
+    private List<Rocket> rockets = new ArrayList<>();
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public List<Bomb> getBombs() {
+        return bombs;
+    }
+
+    public List<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public List<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
     }
 }
