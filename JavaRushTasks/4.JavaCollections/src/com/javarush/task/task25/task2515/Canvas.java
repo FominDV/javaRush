553c32839b79276d4cb5f80c8aea246e8306472d
Dmitriy Fomin <79067773397@yandex.ru>
2021-07-05 19:47:07 +0300

    game7

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,4 +1,39 @@
 package com.javarush.task.task25.task2515;
 
 public class Canvas {
+
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
 }
