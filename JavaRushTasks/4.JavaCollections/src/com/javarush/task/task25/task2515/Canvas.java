830ecf0d24a53823436c5c4631be61293c35e5d9
Dmitriy Fomin <79067773397@yandex.ru>
2021-07-07 22:14:17 +0300

    game9

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -30,6 +30,24 @@ public class Canvas {
         }
     }
 
+    public void clear() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+    }
+
     public int getWidth() {
         return width;
     }
