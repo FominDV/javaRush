7b0e5b7126b77d9e6daab78d397b91dd37e0fb35
Dmitriy Fomin <79067773397@yandex.ru>
2021-07-13 01:26:04 +0300

    game11

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,17 +1,26 @@
 package com.javarush.task.task25.task2515;
 
-public class Bomb extends BaseObject{
-
+/**
+ * Класс для бомбы.
+ */
+public class Bomb extends BaseObject {
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
 
-    public void move(){
+    /**
+     * Отрисовываем себя на холсте.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
         y++;
     }
-
-    public void draw(Canvas canvas){
-        canvas.setPoint(x,y,'B');
-    }
-
 }
--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,7 +1,17 @@
 package com.javarush.task.task25.task2515;
 
 public class Rocket extends BaseObject{
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+
+    public Rocket(double x, double y) {
+        super(x, y, 1);
     }
+
+    public void move(){
+        y--;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.setPoint(x,y,'R');
+    }
+
 }
