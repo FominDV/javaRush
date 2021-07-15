be62621cac03137e2485275e116a9f4fe914f5c4
Dmitriy Fomin <79067773397@yandex.ru>
2021-07-15 18:04:17 +0300

    game12

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,17 +1,27 @@
 package com.javarush.task.task25.task2515;
 
-public class Rocket extends BaseObject{
+/**
+ * Класс для объектов-ракета
+ */
+public class Rocket extends BaseObject {
 
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
-    public void move(){
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
+    }
+
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
+    public void move() {
         y--;
     }
-
-    public void draw(Canvas canvas){
-        canvas.setPoint(x,y,'R');
-    }
-
 }
--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -1,7 +1,15 @@
 package com.javarush.task.task25.task2515;
 
-public class SpaceShip extends BaseObject{
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+public class SpaceShip extends BaseObject {
+
+    private double dx = 0;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
     }
+
+    public void moveLeft(){dx=-1;}
+
+    public void moveRight(){dx=1;}
+
 }
